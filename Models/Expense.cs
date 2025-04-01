87b022ad4cfb798c43af922f0efa2766d86e313d
Sathya158 <sathyag158@gmail.com>
2025-04-01 23:14:33 +0530

    Add project files.

---
+++ Models/Expense.cs
@@ -0,0 +1,10 @@
+﻿namespace ExpenseTrackerAPI.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        public string Category { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
